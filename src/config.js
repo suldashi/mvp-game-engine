a7308dee247e214f11c6ae113c28dd7c35e4ea5a
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-11-01 20:48:42 +0100

    added pixi js and config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const ticksPerSecond = 60;
+const timestep = 1000/ticksPerSecond;
+
+module.exports = {
+    screenWidth: 800,
+    screenHeight: 600,
+    timestep
+}
--- src/engine.js
+++ src/engine.js
@@ -1,5 +1,16 @@
 const MainLoop = require("mainloop.js");
-let ticksSinceStart = 0;
-MainLoop.setUpdate(() => {
-    console.log(ticksSinceStart++ + " ticks since the start of the engine.");
+const PIXI = require("pixi.js");
+const config = require("./config");
+
+const pixiApp = new PIXI.Application({width: config.screenWidth, height: config.screenHeight});
+const parentElement = document.getElementById("root");
+parentElement.appendChild(pixiApp.view);
+
+MainLoop.setSimulationTimestep(config.timestep)
+.setBegin(() => {
+    //handle input here
+}).setUpdate((delta) => {
+    console.log(delta);
+}).setDraw(() => {
+    //handle drawing here
 }).start();
