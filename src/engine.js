db15dc1a139cc949a8f1dbcff538dfa9add2220c
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-11-01 02:26:09 +0100

    added the main loop code

--- src/engine.js
+++ src/engine.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+const MainLoop = require("mainloop.js");
+let ticksSinceStart = 0;
+MainLoop.setUpdate(() => {
+    console.log(ticksSinceStart++ + " ticks since the start of the engine.");
+}).start();
