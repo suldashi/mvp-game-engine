51eb4e08e71f8bd31741c2bdfed75d064ab7f606
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-11-10 18:10:05 +0100

    added more instructions to the setup

--- src/setup/config.js
+++
@@ -1,9 +0,0 @@
-const ticksPerSecond = 60;
-const timestep = 1000/ticksPerSecond;
-
-module.exports = {
-    screenWidth: 600,
-    screenHeight: 600,
-    timestep,
-    DOMContainerElementID: "root"
-}
--- src/setup/engine.js
+++ src/setup/engine.js
@@ -1,1 +1,8 @@
-console.log("hello game");
+const MainLoop = require("mainloop.js");
+
+let loopCounter = 0;
+
+MainLoop.setUpdate(() => {
+    console.log("The game loop has been executed " + loopCounter + " times.");
+    loopCounter++;
+}).start();
