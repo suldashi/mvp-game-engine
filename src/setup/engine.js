8053774d98be50374bd56a6bd586a3601cf46e33
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-11-11 02:56:03 +0100

    finished the intro article

--- src/setup/engine.js
+++ src/setup/engine.js
@@ -1,4 +1,14 @@
 const MainLoop = require("mainloop.js");
+const PIXI = require("pixi.js");
+
+let pixiApp = new PIXI.Application();
+let pixiContainer = document.getElementById("pixi-container");
+let pixiGraphics = new PIXI.Graphics();
+pixiGraphics.beginFill(0x00FF00);
+pixiGraphics.drawPolygon([50,50,200,50,50,200]);
+pixiGraphics.endFill();
+pixiApp.stage.addChild(pixiGraphics);
+pixiContainer.appendChild(pixiApp.view);
 
 let loopCounter = 0;
 
